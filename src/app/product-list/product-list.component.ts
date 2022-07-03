6c57f7386c42e67c755d22c282893bf1871b76c0
eperico <emilien.perico@gmail.com>
2022-07-03 12:10:56 +1000

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
+import { products, Product } from '../products';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  share(product: Product) {
+    window.alert(`The product ${product.name} has been shared!`);
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
